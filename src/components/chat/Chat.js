fd337b03782750f7dc476bbaefe185af64d3f43e
kozimov786 <email@youremail.com>
2022-01-30 17:20:31 +0500

    add item

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ChatItem from './ChatItem';
 import './chat.css'
 import mayoqImg from './../../images/mayoq.jpeg'
 import reactImg from './../../images/reactjs.jpeg'
 import dostImg from './../../images/dostlar.jpeg'
 import lpImg from './../../images/lp-academy.jpeg'
+import itJobsImg from './../../images/itJobs.jpeg'
+
 
 
 const active = true;
@@ -44,6 +46,14 @@ const Chat = () => {
         user="Zafar"
         message="Osh yimizmi endi"
         img={dostImg}
+        time='22:12'
+        badge='2'
+      />
+      <ChatItem
+        title="It Jobs Group"
+        user="Nodirbek"
+        message="Maosh nechpul"
+        img={itJobsImg}
         time='22:12'
         badge='2'
       />
