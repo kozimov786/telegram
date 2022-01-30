0776525a6fd484155f9e27c354562367d276448e
kozimov786 <email@youremail.com>
2022-01-30 13:17:53 +0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrg8M2jA1M7Ca7k9yTSk_r7D4Qis3gTCg",
+  authDomain: "telegram-4e6e1.firebaseapp.com",
+  projectId: "telegram-4e6e1",
+  storageBucket: "telegram-4e6e1.appspot.com",
+  messagingSenderId: "759297276731",
+  appId: "1:759297276731:web:738a9644cbcbaf4ea1551e"
+};
+
+// Initialize Firebasey
+const app = initializeApp(firebaseConfig);
